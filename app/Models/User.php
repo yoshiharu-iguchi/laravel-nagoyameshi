eaecddd542a38287b03ab8ab96845bbdfc4ecb43
iguigu <iguchi2203@gmail.com>
2025-06-11 11:03:42 +0900

    step4 不要なルーティングを削除するまで終了

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,15 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'kana',
         'email',
         'password',
+        'postal_code',
+        'address',
+        'phone_number',
+        'birthday',
+        'occupation',
+        
     ];
 
     /**
